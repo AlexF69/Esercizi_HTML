29603abb5a71137488517e42c57282f4d4ec6601
Alessandra Fiocco <Alessandra.Fiocco@gmail.com>
2022-02-06 18:52:30 +0100

    Esercizio_JS_24

---
+++ exercise-24.js
@@ -0,0 +1,19 @@
+// # Object Methods - Exercise 2
+// Utilizzando l'oggetto `person` stampare in console i suoi valori nel seguente
+// modo utilizzando il metodo `Object.values`:
+
+// ```
+// Mario
+// Rossi
+// 25
+// ```
+
+const person = {
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+// Print values of person using Object.values
+
+console.log(Object.values(person));
